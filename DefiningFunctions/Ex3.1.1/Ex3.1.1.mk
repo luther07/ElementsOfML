57fffbb3f56b8dc729ad3fb84c06242a301b08ef
Mark Johnson <luther07@gmail.com>
2013-08-31 23:18:12 -0500

    Add file which tells the purpose of the files in this directory.

---
+++ DefiningFunctions/Ex3.1.1/Ex3.1.1.mk
@@ -0,0 +1,13 @@
+## Exercise 3.1.1
+
+*DefiningFunctions-open.sml expression to open the structure
+
+*DefiningFunctions.sml - structure containing function definintions
+
+*definingfunctions-sigs.sml - signatures file
+
+*definingfunctions.mlb - ml basis definitions
+
+*definingfunctionstest.mlb - ml basis definitions with test
+
+*test.sml - some tests
